531d1564bcecdb765465f1e75aa285c9a4b5c3cc
knapptan <nikitas@live.ru>
2024-05-26 22:25:27 +0300

    add stack and queue stucts

--- src/A2_SimpleNavigator_Swift/A2_SimpleNavigator_Swift/s21_queue.swift
+++ src/A2_SimpleNavigator_Swift/A2_SimpleNavigator_Swift/s21_queue.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct Queue<Element> {
+    private var elements: [Element] = []
+    
+    // Создание пустой очереди
+    init() {}
+    
+    // Проверка, пуста ли очередь
+    func isEmpty() -> Bool {
+        return elements.isEmpty
+    }
+    
+    // Добавление элемента в конец очереди
+    mutating func push(_ value: Element){
+        elements.append(value)
+    }
+    
+    // Удаление элемента из начала очереди
+    @discardableResult
+    mutating func pop() -> Element? {
+        return elements.removeFirst()
+    }
+    
+    // Получение первого элемента из очереди без его удаления из очереди
+    func front() -> Element? {
+        return elements.first
+    }
+    
+    // Получение последнего элемента из очереди без его удаления из очереди
+    func back() -> Element? {
+        return elements.last
+    }
+    
+    // Получение количества элементов в очереди
+    func count() -> Int {
+        return elements.count
+    }
+    
+}
--- src/A2_SimpleNavigator_Swift/A2_SimpleNavigator_Swift/s21_stack.swift
+++ src/A2_SimpleNavigator_Swift/A2_SimpleNavigator_Swift/s21_stack.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+struct Stack<Element> {
+    private var elements: [Element] = []
+    
+    // Создание пустого стека
+    init() {}
+    
+    // Проверка, пуст ли стек
+    func isEmpty() -> Bool {
+        return elements.isEmpty
+    }
+    
+    // Добавление элемента на вершину стека
+    mutating func push(_ value: Element){
+        elements.append(value)
+    }
+    
+    // Удаление элемента с вершины стека
+    @discardableResult
+    mutating func pop() -> Element? {
+        return elements.popLast()
+    }
+    
+    // Получение с вершины стека без его удаления
+    func top() -> Element? {
+        return elements.last
+    }
+    
+    // Получение количества элементов в стеке
+    func count() -> Int {
+        return elements.count
+    }
+    
+}
